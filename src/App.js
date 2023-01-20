b128cabbb1201fc9cd69d8c447f37c53c4ce08f2
Md Nayeem Asha <nayeemasha703@gmail.com>
2023-01-21 03:15:16 +0600

    Props types

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ function App() {
       <a className="App-link" href="https://www.facebook.com/mdnayeemsarkar03" target="_blank" rel="noopener noreferrer">
         <h2>{name}</h2>
       </a>
-      <Students name="nayeem" age={25} />
-      <Students name="asha" age={23} />
-      <Students name="Bizli" age={22} />
+      <Students name="25" age={25} />
     </div>
   );
 }
--- src/components/Students.js
+++ src/components/Students.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import Common from './Common'
+import propTypes from 'prop-types'
 const Students = (props) => {
   return (
     <div>
-        <h3>{props.name}, {props.age}<Common age={props.age} /></h3>
+        <h3>{props.name}, {props.age}</h3>
+        <Common age={props.age} />
         
     </div>
   )
 }
+
+// Students.prototypes = {
+//     name: propTypes.string,
+//     age: propTypes.array
+// }
+
+Students.propTypes = {
+    name: propTypes.string,
+    age: propTypes.number
+}
+
 
 export default Students
